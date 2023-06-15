0714567d32232422999d0abbaf17812d07fc168a
ARipeAppleByYoursTruly <appleandroid1104@gmail.com>
2023-06-15 18:18:45 +0800

    Added more input validation Added additional command line switches

--- src/edge-pdf.py
+++ src/edge-pdf.py
@@ -1,36 +1,81 @@
-import os, sys
+import sys, os, subprocess
 from tkinter import messagebox
 
 def main():
-    # Check command line syntax
-    argLen = len(sys.argv)
+    argumentsLength: int = len(sys.argv)
 
-    if (argLen == 1):
-        triggerError("Please choose a PDF file, and open with edge-pdf.exe.")
-    elif (argLen > 2):
-        triggerError("Too many command line arguments, please pass in only 1 PDF file.")
-
-
-    # Check arugment's file type
-    fileToOpen = sys.argv[1]
-
-    if (not fileToOpen.endswith(".pdf")):
-        triggerError("edge-pdf.exe can only open PDF files.")
-
-
-    # Replace ' ' in fileToOpen to '%20' because URLs can't contain literal spaces
-    fileToOpen.replace(" ", "%20")
-
-    # Open the file with Microsoft Edge in app mode
-    os.system("start msedge --app=\"" + fileToOpen + "\"")
+    if argumentsLength == 1:
+        showError("Please choose a PDF file, and open with edge-pdf.exe.")
+    elif argumentsLength > 2:
+        showError("Too many command line arguments, please pass in only 1 PDF file.")
 
 
 
-def triggerError(msg):
-    msg += "\n\nFor more info, visit https://github.com/ARipeAppleByYoursTruly/edge-pdf"
+    fileToOpen: str = sys.argv[1]
 
-    messagebox.showerror(title="edge-pdf Error", message=msg)
+    if not os.path.isfile(fileToOpen):
+        showFileOpenError(fileToOpen, "File does not exist.")
+
+    if not fileToOpen.lower().endswith(".pdf"):
+        showFileOpenError(fileToOpen, "edge-pdf.exe can only open PDF files.")
+
+    if not isFilepathAsciiCharactersOnly(fileToOpen):
+        showFileOpenError(fileToOpen, "Filepath contains non-ASCII characters, please rename the file and try again.")
+
+
+
+
+    edgeArgumentList: str = f'"--app=`"{createEncodedURL(fileToOpen)}`" --inprivate --start-maximized"'
+
+    subprocess.run(["powershell", "start", "msedge", edgeArgumentList])
+
+
+
+def isFilepathAsciiCharactersOnly(filepath: str) -> bool:
+    for ch in filepath:
+        if ord(ch) not in range(32, 128):
+            return False
+
+    return True
+
+
+
+def createEncodedURL(filepath: str) -> str:
+    # Windows does not allow these characters in filenames
+    # \ / : * ? " < > |
+
+    url = filepath.replace("%", "%25") # Must encode '%' first
+    url = filepath.replace(" ", "%20")
+    url = filepath.replace("!", "%21")
+    url = filepath.replace("#", "%23")
+    url = filepath.replace("$", "%24")
+    url = filepath.replace("&", "%26")
+    url = filepath.replace("'", "%27")
+    url = filepath.replace("(", "%28")
+    url = filepath.replace(")", "%29")
+    url = filepath.replace("*", "%2A")
+    url = filepath.replace("+", "%2B")
+    url = filepath.replace(",", "%2C")
+    url = filepath.replace(";", "%3B")
+    url = filepath.replace("=", "%3D")
+    url = filepath.replace("@", "%40")
+    url = filepath.replace("[", "%5B")
+    url = filepath.replace("]", "%5D")
+
+    return url
+
+
+
+def showError(message: str):
+    message += "\n\nFor more info, visit https://github.com/ARipeAppleByYoursTruly/edge-pdf"
+
+    messagebox.showerror(title="edge-pdf Error", message=message)
     sys.exit(1)
+
+
+
+def showFileOpenError(filepath: str, message: str):
+    showError(f"Failed to open file specified by the following filepath:\n{filepath}\n\n{message}")
 
 
 
